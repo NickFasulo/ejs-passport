e9e5a1dba29e63b2042dac81be5d0ea3f2537313
[nick_fasulo] <[nick.fasulo@codeimmersives]>
2020-02-19 15:59:02 -0500

    init

---
+++ routes/users.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+
+/* GET users listing. */
+router.get("/", (req, res, next) => {
+  res.send("respond with a resource");
+});
+
+router.get("/login", (req, res) => {
+  res.render("login");
+});
+
+router.get("/register", (req, res) => {
+  res.render("register");
+});
+
+module.exports = router;
